cbf8b45d9cabaa42b670f5e760daeda795b0a54e
윤정민 <58061756+jungmiin@users.noreply.github.com>
2023-04-15 00:29:14 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.py
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -1,35 +1,21 @@
 import heapq
 from collections import defaultdict
+result = []
 
-def dfs(result, N, adj, a1):
+def dfs(adj, a1):
+    global result
+    print(adj)
     result.append(a1)
-    if len(result) == N + 1:
-        return True
-    
-    if a1 not in adj:
-        result.pop()
-        return False
-    
-    for _ in range(len(adj[a1])):
-        a2 = adj[a1].pop()
-        if dfs(result, N, adj, a2):
-            return True
-        adj[a1].insert(0, a2)
-        
-    result.pop()
-    return False
+    while adj[a1]:
+        a2 = heapq.heappop(adj[a1])
+        dfs(adj,a2)
+    return result
 
 def solution(tickets):
     answer = []
-    result = []
     adj = defaultdict(list)
-    
     for ticket in tickets:
         a1, a2 = ticket
-        adj[a1].append(a2)
-        
-    for a in adj.keys():
-        adj[a].sort(reverse=True)
-        
-    dfs(result, len(tickets), adj, "ICN")
-    return result
+        heapq.heappush(adj[a1], a2)
+
+    return dfs(adj, "ICN")
