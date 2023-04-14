72a8600eb26469951a128ccce1992f3206ef534c
윤정민 <58061756+jungmiin@users.noreply.github.com>
2023-04-14 19:19:00 +0900

    [Gold V] Title: 꿀벌 승연이, Time: 756 ms, Memory: 556360 KB -BaekjoonHub

--- 백준/Gold/23083. 꿀벌 승연이/꿀벌 승연이.py
+++ 백준/Gold/23083. 꿀벌 승연이/꿀벌 승연이.py
@@ -3,29 +3,30 @@ input = sys.stdin.readline
 sys.setrecursionlimit(300000)
 
 def main():
-    def make_holes():
-        K = int(input())
+    def k():
+        K = int(input().rstrip())
         for _ in range(K):
             i, j = map(int, input().split())
             dp[i][j] = 0
 
-    def find_dp(i, j):
+    def find(i, j):
         if dp[i][j] != -1: return dp[i][j]
-        val = find_dp(i-1, j)+find_dp(i, j-1)
-        val += find_dp(i-1, j-1) if j%2 else find_dp(i+1, j-1)
-        dp[i][j] = int(val%X)
+        val = find(i-1, j)+find(i, j-1)
+        val += find(i-1, j-1) if j%2 else find(i+1, j-1)
+        dp[i][j] = int(val%(1e9+7))
         return dp[i][j]
 
-    X = 1e9+7
-    I, J = map(int, input().split())
-    dp = [[-1]*(J+1) for _ in range(I+2)]
+    N, M = map(int, input().split())
+    dp = [[-1] * (M+1) for _ in range(N+2)]
     dp[1][1] = 1
-    for i in range(I+1):
+    for i in range(N+2):
         dp[i][0] = 0
-    for j in range(J+1):
-        dp[0][j] = 0
-        dp[I+1][j] = 0
-    make_holes()
-    return find_dp(I, J)
+    for i in range(M+1):
+        dp[0][i] = 0
+        dp[N+1][i] = 0
+    k()
+    find(N, M)
 
-print(main())
+    print(dp[N][M])
+
+main()
